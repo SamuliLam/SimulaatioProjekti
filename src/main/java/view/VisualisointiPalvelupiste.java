a4b2ae6a55e42b46862079e81f79cfa333dccbb6
kismet <kismet1985@hotmail.fi>
2024-02-19 07:28:30 +0200

    Asiakas luokkaan lisätty getterit listojen siirtoon kontrolleriin, ikontrolleriin lisätty metodeja, iSimulaattoriin lisätty metodeja, kontrolleriin lisätty metodit jotka välittää tiedot modelin ja viewin välillä, palvelupisteeseen getterit ja uusi hashmap, Simulaattorigui isoja muutoksia tilastojen näyttöä varten.

---
+++ src/main/java/view/VisualisointiPalvelupiste.java
@@ -0,0 +1,50 @@
+package view;
+
+import javafx.scene.Node;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.chart.PieChart;
+import javafx.scene.layout.StackPane;
+import simu.model.Palvelupiste;
+import simu.model.TapahtumanTyyppi;
+
+import java.util.HashMap;
+import java.util.Map;
+public class VisualisointiPalvelupiste extends StackPane implements IVisualisointi {
+    private PieChart pieChart;
+
+    public VisualisointiPalvelupiste(int w, int h) {
+        super();
+        pieChart = new PieChart();
+        pieChart.setPrefSize(w, h);
+        this.getChildren().add(pieChart);
+    }
+
+    // piechart palvelupisteistä
+    public void updateServicePointVisitData(HashMap<String, Integer> visitData) {
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+
+        for (Map.Entry<String, Integer> entry : visitData.entrySet()) {
+            String label = entry.getKey().toString() + " (" + entry.getValue() + ")";
+            pieChartData.add(new PieChart.Data(label, entry.getValue()));
+        }
+
+        pieChart.setData(pieChartData);
+    }
+
+
+    @Override
+    public void tyhjennaNaytto() {
+
+    }
+
+    @Override
+    public void uusiAsiakas() {
+        // Implement adding a new customer if needed
+    }
+
+    @Override
+    public Node getStyleableNode() {
+        return super.getStyleableNode();
+    }
+}
