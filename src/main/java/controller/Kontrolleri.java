8c89a7a9a21468ed77dec69ae29b9a3772d88478
kismet <kismet1985@hotmail.fi>
2024-02-19 22:10:47 +0200

    Statistiikkanäyttöä parannettu, rahantulostuksia parannettu, koodia siivottu, parametreja helpotettu ja uusi rahankäyttö statistiikka implementoitu. Labelit todennettu myös.

--- src/main/java/controller/Kontrolleri.java
+++ src/main/java/controller/Kontrolleri.java
@@ -34,7 +34,7 @@ public class Kontrolleri implements IKontrolleriForM, IKontrolleriForV{   // UUS
 		ui.getVisualisointi().tyhjennaNaytto();
 		((Thread)moottori).start();
 		//((Thread)moottori).run(); // Ei missään tapauksessa näin. Miksi?
-		HashMap<Integer, Integer> ageDistribution = asiakas.getAgeDistribution();
+		HashMap<Integer, Integer> ageDistribution = Asiakas.getAgeDistribution();
 		// Lähetä ikäjakauma view modeliin
 		ui.updateAgeDistribution(ageDistribution);
 	}
@@ -51,8 +51,11 @@ public class Kontrolleri implements IKontrolleriForM, IKontrolleriForV{   // UUS
 	}
 
 	public HashMap<String, Integer> getPalvelupisteDistribution() {
-		HashMap<String, Integer> palvelupisteDistribution = Palvelupiste.getPalveluLuku();
-		return palvelupisteDistribution;
+        return Palvelupiste.getPalveluLuku();
+	}
+
+	public HashMap<Asiakas, Double> getSpentMoneyDistribution() {
+        return Asiakas.getSpentmoneyPerAsiakas();
 	}
 
 	@Override
